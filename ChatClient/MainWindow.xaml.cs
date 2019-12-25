f141277132281645cc47a73022cf91e9d5fcf39f
makishev@yahoo.com <makishev@yahoo.com>
2019-12-25 21:44:10 +0600

    chat client

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List <Message> Messages;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Messages = new List<Message>
+            {
+                new Message
+                {
+                    From = "Essss",
+                    Text = "na xxxxx"
+                },
+                new Message
+                {
+                    From = "wewwew",
+                    Text = "asd"
+                },
+                new Message
+                {
+                    From = "qweqwe",
+                    Text = "asdasd"
+                }
+            };
+            itemsControl.ItemsSource = Messages;
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Domain/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Message
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string From { get; set; }
+        public DateTime SendTime { get; set; } = DateTime.Now;
+        public string Text { get; set; }
+    }
+}
